49c4ad836da7c9533fc19ec3421e20f1e9bd864e
joey <magentodevuk@gmail.com>
2024-04-18 01:19:07 +0100

    Update NavBar component to include Dashbord link and remove duplicate link

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -32,13 +32,9 @@ const NavBar = async () => {
               <Link href='/Dashbord'>
                 <li>Dashbord</li>
               </Link>
-              <li><UserButton afterSignOutUrl="/" /></li>
             </ul>
           </div>
          <ul className="flex flex-auto justify-end gap-5 text-white ">
-              <Link href='/Dashbord'>
-                <li>Dashbord</li>
-              </Link>
               <li><UserButton afterSignOutUrl="/" /></li>
               <Link href='/sign-in'>
                 <li>Sign In</li>  
